f9e0d66b08084af0eee3329dfacdb6454a004ea0
Pawandeep Suryavanshi <56802662+PawanSuryavanshi95@users.noreply.github.com>
2022-10-17 01:04:00 +0530

    Unit testing

--- app/src/test/java/com/example/vkalculator2o/ExampleUnitTest.java
+++ app/src/test/java/com/example/vkalculator2o/ExampleUnitTest.java
@@ -9,9 +9,12 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
+// unit testing
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+		assertEquals(2*57, 114);
     }
 }
