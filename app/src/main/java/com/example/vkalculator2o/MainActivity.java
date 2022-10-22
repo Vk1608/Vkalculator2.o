85de9d538f9265534ec62f89d0addd2ab9d2248a
tushar <tss130204@gmail.com>
2022-10-22 12:30:33 +0530

    reverse function added

--- app/src/main/java/com/example/vkalculator2o/MainActivity.java
+++ app/src/main/java/com/example/vkalculator2o/MainActivity.java
@@ -324,7 +324,15 @@ public class MainActivity extends AppCompatActivity {
         }
         return ans;
     }
-
+    static int reversNumber(int n)
+    {
+        int reversed_n = 0;
+        while (n > 0) {
+            reversed_n = reversed_n * 10 + n % 10;
+            n = n / 10;
+        }
+        return reversed_n;
+    }
     static double medianCal(int  n,double in[])
 	{
 		double m=0;	
