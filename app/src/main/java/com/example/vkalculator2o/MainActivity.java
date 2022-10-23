88f0c103d5c974e95a122dda1dce41a25fb7b674
vivek <chota8930361400@gmail.com>
2022-10-23 10:23:20 +0530

    pascal

--- app/src/main/java/com/example/vkalculator2o/MainActivity.java
+++ app/src/main/java/com/example/vkalculator2o/MainActivity.java
@@ -286,6 +286,24 @@ public class MainActivity extends AppCompatActivity {
             array[j] = a;  
         }  
     } 
+
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+		List<Integer> row, pre = null;
+		for (int i = 0; i < numRows; ++i) {
+			row = new ArrayList<Integer>();
+			for (int j = 0; j <= i; ++j)
+				if (j == 0 || j == i)
+					row.add(1);
+				else
+					row.add(pre.get(j - 1) + pre.get(j));
+			pre = row;
+			res.add(row);
+		}
+		return res;
+    }
+
+
     public static double myPow(double x, int n) {
     double ans = 1.0;
     long nn = n;
