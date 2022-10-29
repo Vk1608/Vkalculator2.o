38e80aca72f684c65664266385646fdc36e4375d
Vikram <vk1608rocks@gmail.com>
2022-10-29 13:50:45 +0530

    edit distance

--- app/src/main/java/com/example/vkalculator2o/MainActivity.java
+++ app/src/main/java/com/example/vkalculator2o/MainActivity.java
@@ -323,7 +323,35 @@ static int knapsack(int[] wt,int[] val, int n, int W){
     return knapsackUtil(wt, val, n-1, W, dp);
 }
 
+static int editDistanceUtil(String S1, String S2, int i, int j, int[][] dp){
+    
+    if(i<0)
+        return j+1;
+    if(j<0)
+        return i+1;
+        
+    if(dp[i][j]!=-1) return dp[i][j];
+        
+    if(S1.charAt(i)==S2.charAt(j))
+        return dp[i][j] =  0+editDistanceUtil(S1,S2,i-1,j-1,dp);
+        
+    // Minimum of three choices
+    else return dp[i][j] = 1+Math.min(editDistanceUtil(S1,S2,i-1,j-1,dp),
+    Math.min(editDistanceUtil(S1,S2,i-1,j,dp),editDistanceUtil(S1,S2,i,j-1,dp)));
+    
+}
 
+static int editDistance(String S1, String S2){
+    
+    int n = S1.length();
+    int m = S2.length();
+    
+    int[][] dp=new int[n][m];
+    for(int row[]: dp)
+    Arrays.fill(row,-1);
+    return editDistanceUtil(S1,S2,n-1,m-1,dp);
+    
+}
 
     //Factorial Function
     int factorial(int n)
